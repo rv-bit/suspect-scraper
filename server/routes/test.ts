5c2d0a3e491c5645975f2480dd82d7c758b59be7
Robert Vilcu <robertvilcu89@gmail.com>
2025-04-16 21:13:43 +0100

    feat: return all unique places from crime data

--- server/routes/test.ts
+++ server/routes/test.ts
@@ -1,9 +1,24 @@
 import { Hono } from 'hono'
 
+import { db } from '~/server/database/index'
+import { mySchemaCrimeData } from '~/server/database//schema'
+
 const testRouter = new Hono()
 
+const cacheAreaMap = new Map()
+
 testRouter.get('/', async (c) => {
-	return c.json({ message: 'Hello, World!' })
+	const key = c.req.url
+	if (cacheAreaMap.has(key)) {
+		return c.json({	areas: cacheAreaMap.get(key) })
+	}
+
+	const areas = await db.selectDistinct({ fallsWithin: mySchemaCrimeData.fallsWithin }).from(mySchemaCrimeData).orderBy(mySchemaCrimeData.fallsWithin);
+	const areaNames = areas.map((area) => area.fallsWithin);
+
+	cacheAreaMap.set(key, areaNames)
+
+	return c.json({ areas: areaNames })
 })
 
 export default testRouter
