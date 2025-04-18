099fd5e37338cf85cc2cb212e96c29d29e2a6160
Robert Vilcu <robertvilcu89@gmail.com>
2025-04-18 21:43:11 +0100

    chore: add utility functions

--- frontend/app/lib/utils.ts
+++ frontend/app/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
+
+export const titleCase = (str: string) => {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    return splitStr.join(' '); 
+}
+
+export const formatMonth = (month: string) => {
+    const [year, monthIndex] = month.split("-").map(Number);
+    const date = new Date(year, monthIndex - 1);
+    return date.toLocaleString("en-US", { month: "short", year: "numeric" });
+}
