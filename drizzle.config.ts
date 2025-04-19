c9d36fdc0126e53634228f46eedf3077521762d2
Robert Vilcu <robertvilcu89@gmail.com>
2025-04-20 00:16:03 +0100

    chore: moved location of schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
 	out: './drizzle',
-	schema: './server/database/schema.ts',
+	schema: './server/database/schema/schema.ts',
 	dialect: 'postgresql',
 	dbCredentials: {
 		url: process.env.DATABASE_URL!,
